0847a3c1f029012b17c3876480eda3816a065dfa
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-04-03 23:46:46 +1000

    First commit of < 4-3>  <23:42> : create vite project using a vanilla javascript template;

---
+++ vite-project/src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import './main.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+
+
+setupCounter(document.querySelector('#counter'))
